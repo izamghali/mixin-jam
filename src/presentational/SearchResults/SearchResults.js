9720df2a66b67f952b950a1394e0b0f1505bb2e6
Izam Ghali <izamghali18@gmail.com>
2023-08-06 00:14:34 +0700

    created SearchResults.js & SearchResults.scss

---
+++ src/presentational/SearchResults/SearchResults.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './SearchResults.scss'
+
+const test = <h3>This is search results</h3>
+
+export function SearchResults() {
+    return (
+        <>
+        <div id='SearchResults'>
+            { test }
+        </div>
+        </>
+    )
+}
