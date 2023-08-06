f3a17dd84f1703a4d162d393da2f6c23bc74f114
Izam Ghali <izamghali18@gmail.com>
2023-08-06 16:19:12 +0700

    deleted default App.scss style & restructured it from scratch

--- src/App.js
+++ src/App.js
@@ -8,21 +8,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <h1>Mixin' Jam</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
 
-      <SearchBar />
-      <SearchResults>
-      </SearchResults>
+      <div className='Body'> 
+        <SearchBar />
+      </div>
+        <SearchResults>
+        </SearchResults>
       <Track />
     </div>
   );
