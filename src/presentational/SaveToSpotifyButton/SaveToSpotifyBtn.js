8c22acb328f669482734a8966a920d30dee6deea
Izam Ghali <izamghali18@gmail.com>
2023-08-07 13:27:48 +0700

    created SaveToSpotifyBtn.js & SaveToSpotifyBtn.scss

---
+++ src/presentational/SaveToSpotifyButton/SaveToSpotifyBtn.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export function SaveToSpotifyBtn() {
+    return (
+        <>
+            <form>
+                    <input
+                    type='submit' 
+                    value='Save to Spotify'
+                    ></input>
+            </form>
+        </>
+    )
+}
