e5c6bdccab1ef1e8a2f4682d53a2eefd3af5c83a
Izam Ghali <izamghali18@gmail.com>
2023-08-11 17:36:55 +0700

    added clientId & clientSecret to request access token for Spotify API call

--- src/container/Card/Card.js
+++ src/container/Card/Card.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { CardPresentational } from '../../presentational/Card/CardPresentational';
 
 export function Card() {
+
+    const clientId = '89cc9f4988ea4c7985a164bf3392cd1d';
+    const clientSecret = 'f1348b92b74240898b500661ba3339d5';
+
+    useEffect(() => {
+        // API access token
+    }, [])
+
     return (
         <>
             <CardPresentational/>
