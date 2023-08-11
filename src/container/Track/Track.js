2ce4a59de746092a2273616203a660e71496da87
Izam Ghali <izamghali18@gmail.com>
2023-08-12 05:26:13 +0700

    made API call to get Spotify access token

--- src/container/Track/Track.js
+++ src/container/Track/Track.js
@@ -4,14 +4,30 @@ import { TrackPresentational } from '../../presentational/Track/TrackPresentatio
 
 export function Track() {
 
-    const clientId = '89cc9f4988ea4c7985a164bf3392cd1d';
-    const clientSecret = 'f1348b92b74240898b500661ba3339d5';
+    const [ accessToken, setAccessToken ] = useState('')
+
+    const CLIENT_ID = '89cc9f4988ea4c7985a164bf3392cd1d';
+    const CLIENT_SECRET = 'f1348b92b74240898b500661ba3339d5';
 
     const testArr = [1, 2, 3, 4]
 
     useEffect(() => {
-        // API access token
+        // API Access Token
+        const tokenUrl = 'https://accounts.spotify.com/api/token'
+        const authParameters = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: 'grant_type=client_credentials&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET
+        }
+        fetch(tokenUrl, authParameters)
+            .then(response => response.json())
+            .then(data => setAccessToken(data.access_token))
     }, [])
+
+    console.log(accessToken)
+
 
     return (
         <>
