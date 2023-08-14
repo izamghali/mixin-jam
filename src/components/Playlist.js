7318c6247b5a8c8caf91b0e2ff0e094efaa08265
Izam Ghali <izamghali18@gmail.com>
2023-08-14 16:14:34 +0700

    deleted margin-bottom of Album component at Album.scss, worked on Playlist.js component & scss

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -1,8 +1,29 @@
+import React, { useState } from 'react';
+import './Playlist.scss';
 
 export function Playlist() {
+
+    let dummyText = 'New Playlist'
+
+    const [ playlistTitle, setPlaylistTitle ] = useState('')
+
+    const handleChange = ({target}) => {
+        setPlaylistTitle(target.value)
+    }
+
     return (
         <>
-            <h3>This is playlist</h3>
+            <div className='Playlist'>
+                <form>
+                    <input
+                        type='text'
+                        value={playlistTitle}
+                        onChange={handleChange}
+                        >
+                    </input>
+                </form>
+                <h3>This is playlist</h3>
+            </div>
         </>
     )
 }
