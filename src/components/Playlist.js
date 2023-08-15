90685207d6f6b4f6a994f397050a1e181c7f3303
Izam Ghali <izamghali18@gmail.com>
2023-08-15 15:23:50 +0700

    created CompactTrack for Playlist. Added props.children at Playlist.js

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './Playlist.scss';
 
-export function Playlist() {
+export function Playlist(props) {
 
     let dummyText = 'New Playlist'
 
@@ -23,7 +23,11 @@ export function Playlist() {
                         onChange={handleChange}
                         >
                     </input>
-                    <button 
+                </form>
+                <div>
+                    { props.children }
+                </div>
+                <button 
                         className='playlist-submit'
                         type='submit'
                         value=''
@@ -31,8 +35,7 @@ export function Playlist() {
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
                         <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"/>
                     </svg>Save to Spotify
-                    </button>
-                </form>
+                </button>
             </div>
         </>
     )
