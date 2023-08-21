bac1c3d5fa7fccc2e55b92e842499fb1e0778667
Izam Ghali <izamghali18@gmail.com>
2023-08-21 19:58:57 +0700

    added inner div on #CompactTrack component & padding on it

--- src/components/CompactTrack.js
+++ src/components/CompactTrack.js
@@ -18,15 +18,17 @@ export function CompactTrack(props) {
     return (
         <>
             <div className='CompactTrack' onClick={removeTrack}>
-                <div className='compact-content'>
-                    <h3>{props.trackTitle}</h3>
-                    <p>{props.artistName}</p>
+                <div className='compact-track-inner-div'>
+                    <div className='compact-content'>
+                        <h3>{props.trackTitle}</h3>
+                        <p>{props.artistName}</p>
+                    </div>
+                    <button>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dash-lg" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M2 8a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11A.5.5 0 0 1 2 8Z"/>
+                        </svg>
+                    </button>
                 </div>
-                <button>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dash-lg" viewBox="0 0 16 16">
-                        <path fill-rule="evenodd" d="M2 8a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11A.5.5 0 0 1 2 8Z"/>
-                    </svg>
-                </button>
             </div>
         </>
     )
