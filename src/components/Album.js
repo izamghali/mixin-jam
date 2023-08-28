502048c1b1eeedf736454f600b15a7e73d8e8476
Izam Ghali <izamghali18@gmail.com>
2023-08-28 20:21:19 +0700

    added ShadowAlbum class as a parent class of Album class, managed to create shadow effect when hovered

--- src/components/Album.js
+++ src/components/Album.js
@@ -16,19 +16,14 @@ export function Album(props) {
 
     return (
         <>
-        <div className='Album' onClick={handleClick}>
-            <div className='album-img-div'>
-                <img src={albumImgSrc} alt="" />
+        <div className='ShadowAlbum'>
+            <div className='Album' onClick={handleClick}>
+                <div className='album-img-div'>
+                    <img src={albumImgSrc} alt="" />
+                </div>
+                <h3 className='album-title'>{albumNames}</h3>
             </div>
-            <h3>{albumNames}</h3>
-            {/* <button>
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
-                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"/>
-                </svg>
-                View Album
-            </button> */}
         </div>
-        <div className='ShadowAlbum'></div>
         </>
     )
 }
