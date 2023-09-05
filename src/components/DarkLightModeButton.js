7bab4d999798d1bdc16c2837412c4f1300f06814
Izam Ghali <izamghali18@gmail.com>
2023-09-05 23:42:40 +0700

    created DarkLightModeButton component, shaped it, & added its toggle slide effect

---
+++ src/components/DarkLightModeButton.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import './DarkLightModeButton.scss'
+import gsap from 'gsap';
+
+export function DarkLightModeButton() {
+
+    const [ toggleState, setToggleState ] = useState(true);
+
+    const handleClick = (event) => {
+        if (toggleState) {
+            gsap.to('.toggle-button', { x: '100%', duration: 0.4, })
+        } else {
+            gsap.to('.toggle-button', { x: '-100%', duration: 0.4, })
+        }
+        setToggleState(!toggleState)
+    }
+
+    return (
+        <>
+            <div className='DarkLightModeButton' onClick={handleClick}>
+                <div className='toggle-button'></div>
+            </div>
+        </>
+    )
+}
--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { LoginSpotifyButton } from './LoginSpotifyButton';
+import { DarkLightModeButton } from '../DarkLightModeButton';
 import './LoginPage.scss'
 
 export function LoginPage(props) {
@@ -15,7 +16,10 @@ export function LoginPage(props) {
                     <img src='https://images.unsplash.com/photo-1615554851544-e6249b92a492?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2970&q=80' />
                 </div>
                 <div className='login-content'>
-                    <h1>Mixin' Jam</h1>
+                    <div className='login-content-header'>
+                        <h1>Mixin' Jam</h1>
+                        <DarkLightModeButton />
+                    </div>
                     <p>Mixin' Jam is a React web app where you can search songs, artists, and albums, from Spotify.</p>
                     <div className='login-spotify-button'>
                         <LoginSpotifyButton
