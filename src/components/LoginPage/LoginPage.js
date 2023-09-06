3785da4830feb2f38d8c55a964ae91a76445704d
Izam Ghali <izamghali18@gmail.com>
2023-09-06 14:47:09 +0700

    cleaning up & applied absolute position on login-slogan. Successfully made LoginPage 100% responsive. Great job

--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -16,15 +16,15 @@ export function LoginPage(props) {
                     <img src='https://images.unsplash.com/photo-1615554851544-e6249b92a492?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2970&q=80' />
                 </div>
                 <div className='login-content'>
-                    <div className='login-content-header'>
-                        <h1>Mixin' Jam</h1>
-                        <DarkLightModeButton className="login-dark-light-button"/>
+                    <div className='login-body'>
+                        <div className='login-content-header'>
+                            <h1>Mixin' Jam</h1>
+                            <DarkLightModeButton className="login-dark-light-button"/>
+                        </div>
+                        <p>Mixin' Jam is a React web app where you can search songs, artists, and albums, from Spotify.</p>
                     </div>
-                    <p>Mixin' Jam is a React web app where you can search songs, artists, and albums, from Spotify.</p>
                     <div className='login-spotify-button'>
-                        <LoginSpotifyButton
-                            clientId={clientId}
-                        />
+                        <LoginSpotifyButton clientId={clientId} />
                     </div>
                 </div>
             </div>
