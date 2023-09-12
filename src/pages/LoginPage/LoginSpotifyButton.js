b8ed24cc676475e0e7b89b896ba0af2935b73a3b
Izam Ghali <izamghali18@gmail.com>
2023-09-12 22:07:04 +0700

    deleted PKCE authorization method

--- src/pages/LoginPage/LoginSpotifyButton.js
+++ src/pages/LoginPage/LoginSpotifyButton.js
@@ -13,137 +13,6 @@ export function LoginSpotifyButton(props) {
     const [ authCode, setAuthCode ] = useState('');
     const [ authState, setAuthState ] = useState('');
 
-    const handleClick = async (event) => {
-        event.preventDefault();
-        // generating code verifier
-        let generateRandomString = (length) => {
-            let text = '';
-            let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-          
-            for (let i = 0; i < length; i++) {
-              text += possible.charAt(Math.floor(Math.random() * possible.length));
-            }
-            return text;
-        }
-
-        // hashing code verifier using SHA256 algorithm
-        let generateCodeChallenge = async (codeVerifier) => {
-            function base64encode(string) {
-              return btoa(String.fromCharCode.apply(null, new Uint8Array(string)))
-                .replace(/\+/g, '-')
-                .replace(/\//g, '_')
-                .replace(/=+$/, '');
-            }
-          
-            const encoder = new TextEncoder();
-            const data = encoder.encode(codeVerifier);
-            const digest = await window.crypto.subtle.digest('SHA-256', data);
-          
-            return base64encode(digest);
-        }
-        
-        let codeVerifier = generateRandomString(128);
-
-        const redirectUri = 'http://localhost:3000/home';
-
-        let state = generateRandomString(16);
-        let scope = 'user-read-private user-read-email';
-        localStorage.setItem('code_verifier', codeVerifier);
-
-        let args = new URLSearchParams({
-            response_type: 'code',
-            client_id: clientId,
-            scope: scope,
-            redirect_uri: redirectUri,
-            state: state,
-            code_challenge_method: 'S256',
-            code_challenge: await generateCodeChallenge(codeVerifier)
-        });
-
-        window.location = `https://accounts.spotify.com/authorize?${args}`;
-    }
-
-    const getAccessToken = async (event) => {
-        event.preventDefault();
-
-        let authState = new URLSearchParams(window.location.search);
-        let authCode = authState.get('code');
-        setAuthCode(authCode);
-        setAuthState(authState);
-
-        // Request Access Token
-        let bodyParams = new URLSearchParams({
-            grant_type: 'authorization_code',
-            code: authCode,
-            redirect_uri: 'http://localhost:3000/home',
-            client_id: clientId,
-            code_verifier: localStorage.getItem('code_verifier')
-        });
-
-        let accessTokenParams = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            }, 
-            body: bodyParams
-        }
-
-        // POST request to store the access token
-        try {
-            let response = await (await fetch('https://accounts.spotify.com/api/token', accessTokenParams)).json();
-            localStorage.setItem('access_token', response.access_token)
-        }
-        catch(error) {
-            console.error('Error:', error);
-        }
-        // const refreshParam = new URLSearchParams(window.location.search);
-        // let refreshToken = refreshParam.get('refresh_token');
-
-        // console.log(refreshParam)
-        console.log(localStorage)
-    }
-
-    const handleCheckProfile = async (event) => {
-        event.preventDefault();
-
-        let getProfile = async() => {
-            let accessToken = localStorage.access_token;
-          
-            const response = await fetch('https://api.spotify.com/v1/me', {
-              headers: {
-                Authorization: 'Bearer ' + accessToken
-              }
-            });
-          
-            let data = await response.json();
-            return data;
-        }
-
-        let currentProfile = await getProfile();
-        console.log(currentProfile)
-    
-    }
-
-    const handleRefreshAccessToken = async (event) => {
-        event.preventDefault();
-
-        // refresh access token
-        const refreshAccessToken = await fetch('https://accounts.spotify.com/api/token', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            },
-            body: {
-                grant_type: 'refresh_token',
-                refresh_token: authCode, // this is not authCode, it should be refresh_token returned after authorziation code exchange.
-                client_id: clientId
-            }
-        })
-
-        console.log(refreshAccessToken)
-        console.log(localStorage)
-    }
-
     const handleAuthorize = async(event) => {
         // Implicit grantFlow
         event.preventDefault();
@@ -157,25 +26,15 @@ export function LoginSpotifyButton(props) {
             return text;
         }
 
-        // const searchParams = {
-        //     method: 'GET',
-        //     headers: {
-        //         'Content-Type': 'application/json',
-        //         'Authorization': 'Bearer ' + accessToken,
-        //         'Access-Control-Allow-Origin': 'no-cors'
-        //     }
-        // }
         const searchParams = {
             method: 'GET',
             headers: {
-                'Content-Type': 'application/json',
-                // 'Authorization': 'Bearer ' + accessToken,
-                // 'Access-Control-Allow-Origin': '*',
+                'Content-Type': 'application/x-www-form-urlencoded',
             },
         }
 
         var url = 'https://accounts.spotify.com/authorize';
-        var redirectUri = 'http://localhost:3000/';
+        var redirectUri = 'http://localhost:3000/home';
         var scope = 'user-read-private user-read-email';
         var state = generateRandomString(16);
         // localStorage.setItem(stateKey, state);
@@ -208,27 +67,6 @@ export function LoginSpotifyButton(props) {
                     >
                 </input>
             </form>
-            {/* <form onClick={handleCheckProfile}>
-                <input
-                    type='submit'
-                    value='Check profile'
-                    >
-                </input>
-            </form>
-            <form onClick={getAccessToken}>
-                <input
-                    type='submit'
-                    value='Get Access Token'
-                    >
-                </input>
-            </form>
-            <form onClick={handleRefreshAccessToken}>
-                <input
-                    type='submit'
-                    value='Refresh Access Token'
-                    >
-                </input>
-            </form> */}
         </>
     )
 }
