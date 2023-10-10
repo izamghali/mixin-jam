eee80323cad98e2dca492775c637faca1f9ccecb
Izam Ghali <izamghali18@gmail.com>
2023-10-10 20:34:44 +0700

    successfully made POST request to add items to the new playlist & saved it into spotify account. We did it

--- src/components/CustomPlaylist.js
+++ src/components/CustomPlaylist.js
@@ -16,7 +16,7 @@ export function CustomPlaylist(props) {
         setPlaylistTitle(target.value)
     }
 
-    const savePlaylistTest = async () => {
+    const exportToSpotify = async () => {
 
         const searchParams = {
             headers: {
@@ -24,57 +24,34 @@ export function CustomPlaylist(props) {
                 'Authorization': 'Bearer ' + localStorage.getItem('access_token')
             },
             method: 'POST',
-            body: JSON.stringify({
+        }
+        
+        let newPlaylistID = "";
+        // create new playlist
+        try {
+            searchParams.body = JSON.stringify({
                 name: "Mixin Jam Playlist Test",
                 description: "playlist description bla bla bla bla...",
                 collaborative: false,
                 public: true
             })
-        }
-
-        try {
-            const response = await fetch(`https://api.spotify.com/v1/users/${user_id}/playlists`, searchParams);
-            console.log(response)
-            console.log(await response.json())
+            const response = await (await fetch(`https://api.spotify.com/v1/users/${user_id}/playlists`, searchParams)).json();
+            newPlaylistID = response.id;
         } catch(error) {
             console.log(error)
         }
-    }
 
-    const savePlaylist = async (event) => {
-        event.preventDefault();
-        // console.log("your playlist is saved!");
-        const searchParams = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + localStorage.getItem('access_token')
-            },
+        // add tracks to the playlist
+        try {
+            searchParams.body = JSON.stringify ({
+                uris: addedTracks.map(track => { return `spotify:track:${track.trackID}` }),
+                position: 0,
+            })
+            const response = await (await fetch(`https://api.spotify.com/v1/playlists/${newPlaylistID}/tracks`, searchParams)).json();
+            console.log(response)
+        } catch(error) {
+            console.log(error)
         }
-
-        const createPlaylist = async () => {
-
-            searchParams.body = {
-                "name": "Mixin Jam Playlist Test",
-                "description": "playlist description",
-                "public": true
-            }
-    
-            try {
-                const request = await fetch(`https://api.spotify.com/v1/users/${user_id}/playlists`, searchParams);
-                console.log(request)
-            } catch(error) {
-                console.log(error)
-            }
-
-        }
-
-        const addItemsToPlaylist = async () => {
-
-
-            const request = await fetch(`https://api.spotify.com/v1/playlists/{playlist_id}/tracks`)
-        }
-
     }
 
     const getUserPlayslist = async () => {
@@ -84,6 +61,7 @@ export function CustomPlaylist(props) {
         } catch(error) {
             console.log(error)
         }
+        console.log(addedTracks)
     }
 
     let clicked = 0;
@@ -132,7 +110,7 @@ export function CustomPlaylist(props) {
                 <button className='playlist-submit'
                         type='submit'
                         value=''
-                        onClick={savePlaylistTest}
+                        onClick={exportToSpotify}
                         >
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
                         <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"/>
