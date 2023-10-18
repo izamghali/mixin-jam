08400241afda18006f82464e85e113578b20be31
Izam Ghali <izamghali18@gmail.com>
2023-10-18 23:43:30 +0700

    added color palette #4537 to _variables.scss & dark light mode color palettes on darkModeOn functions as variables

--- src/components/DarkLightModeButton.js
+++ src/components/DarkLightModeButton.js
@@ -4,20 +4,43 @@ import gsap from 'gsap';
 
 export function DarkLightModeButton() {
 
+    // dark mode color palette
+    const darkRegularBlack = '#1D0F0F';
+    const darkPlum = '#453C41';
+    const darkGrey = '#7B7C81';
+    const darkLightSlate = '#D4DBE2';
+    const darkEggplant = '#7B586B';
+
+    // light mode color palette
+    const lightKhaki = '#c2b2a5';
+    const lightEmerald = '#a3c1b4';
+    const lightGrass = '#7a8b65';
+    const lightDarkGrass = '#424f35';
+    const lightBlack = '#25312f';
+
     const [ toggleState, setToggleState ] = useState(true);
 
-    const handleClick = () => {
+    const darkModeOn = () => {
         if (toggleState) {
             gsap.to('.toggle-button', { x: '100%', duration: 0.4, })
+
+            // update dark mode color change
+            gsap.to(".Body", { backgroundColor: darkRegularBlack, })
+            gsap.to(".NavBar", { backgroundColor: darkPlum, })
+
         } else {
             gsap.to('.toggle-button', { x: '-100%', duration: 0.4, })
+
+            // update light mode color change
         }
         setToggleState(!toggleState)
+
+        
     }
 
     return (
         <>
-            <div className='DarkLightModeButton' onClick={handleClick}>
+            <div className='DarkLightModeButton' onClick={darkModeOn}>
                 <div className='toggle-button'></div>
             </div>
         </>
